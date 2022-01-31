1696b2e45f0ee99dca1d976e49e92e0c970868af
AmgadElsayed2021 <amgad.elsayed@icloud.com>
2022-01-31 18:34:01 -0330

    work on other routes

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -23,12 +23,20 @@ app.use(bodyParser.json());
 // app.post("/hello", (req, res) => res.send(`hello ${req.body.name}`));
 // app.get("/hello/:name", (req, res) => res.send(`hello ${req.params.name}`));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// creat a data base connection function to save the code writing
+async function DBConnection() {
+  const client = await MongoClient.connect("mongodb://localhost:27017", {
+    useNewUrlParser: true,
+  });
+  const db = client.db("my-movies");
+  return { db, client };
+}
+
 app.get("/api/movies", async (req, res) => {
   try {
-    const client = await MongoClient.connect("mongodb://localhost:27017", {
-      useNewUrlParser: true,
-    });
-    const db = client.db("my-movies");
+    const { db, client } = await DBConnection();
+
     const movie = await db.collection("movies").find({}).toArray();
     console.log(movie);
     res.status(200).json(movie);
@@ -39,13 +47,32 @@ app.get("/api/movies", async (req, res) => {
 });
 // const Amgad = 3;
 
+app.post("/api/addAMovie", async (req, res) => {
+  try {
+    const { db, client } = await DBConnection();
+    await db.collection("movies").insertOne({
+      Title: req.body.Title,
+      Rating: req.body.Rating,
+      Released: req.body.Released,
+      Actors: req.body.Actors,
+      Poster: req.body.Poster,
+    });
+
+    res
+      .status(200)
+      .json({
+        message: `${req.body.Title} movie has been added to the DataBase`,
+      });
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to db", error });
+  }
+});
+
 app.post("/api/delete", async (req, res) => {
   try {
     //  start db connection
-    const client = await MongoClient.connect("mongodb://localhost:27017", {
-      useNewUrlParser: true,
-    });
-    const db = client.db("movies");
+    const { db, client } = await DBConnection();
 
     let delMovie = await db
       .collection("movies")
@@ -54,12 +81,10 @@ app.post("/api/delete", async (req, res) => {
 
     if (delMovie.deletedCount > 0) {
       const movie = await db.collection("movies").find({}).toArray();
-      res
-        .status(200)
-        .json({
-          message: `${req.body.Title} movie has been deleted`,
-          movies: movie,
-        });
+      res.status(200).json({
+        message: `${req.body.Title} movie has been deleted`,
+        movies: movie,
+      });
     } else {
       res.status(200).json({ message: "can't find the movie name" });
     }
@@ -70,4 +95,20 @@ app.post("/api/delete", async (req, res) => {
   }
 });
 
+app.get("/api/Movie/:Title", async (req, res) => {
+  console.log(req.params.Title);
+  try {
+    const { db, client } = await DBConnection();
+
+    const movie = await db
+      .collection("movies")
+      .find({ Title: req.params.Title })
+      .toArray();
+    console.log(movie);
+    res.status(200).json(movie);
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to db", error });
+  }
+});
 app.listen(8000, () => console.log("App is listening on port 8000"));
